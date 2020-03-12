35420a066579f8419ca9cacf36edc75c67f7725f
Sabrina <sabrina.lewis@bryanuniversity.edu>
2020-03-12 14:08:12 -0500

    submit events update

--- week5/submit_events/index.js
+++ week5/submit_events/index.js
@@ -4,9 +4,9 @@ form.addEventListener("submit", inputAlert);
 
 function inputAlert(event){
   event.preventDefault();
-  const fNameInput = form.fName.value;
-  const lNameInput = form.lName.value;
-  const emailInput = form.email.value;
+  let fNameInput = form.fName.value;
+  let lNameInput = form.lName.value;
+  let emailInput = form.email.value;
   form.fName.value = "";
   form.lName.value = "";
   form.email.value = "";
